1237b29aa9ee55f9b2a763a63ac1fcb0781e62f5
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-09-12 16:30:44 +0200

    Initial commit

---
+++ src/pages/App.jsx
@@ -0,0 +1,14 @@
+import "../styles/App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center">
+        <h1 className="text-5xl text-red-700 mx-auto">JSframeworks</h1>
+        <p className="mx-auto">Testing the Tailwind with Vite setup</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
