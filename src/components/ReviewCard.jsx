938a93012f289633fb5effd406170d75017d5b83
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-09-21 13:24:35 +0200

    adjusts single product page

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -2,7 +2,7 @@ import StarRating from "./StarRating";
 
 function ReviewCard({username, description, rating}) {
   return (
-    <div className="bg-ecom-white flex-none w-[20rem] p-4 rounded-sm shadow-md">
+    <div className="bg-ecom-white flex-none w-[18rem] p-4 rounded-sm shadow-md">
         <div className="flex justify-between items-center">
           <p className="font-semibold">{username}</p>
           <StarRating rating={rating} color="text-orange-400"/>
--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -11,14 +11,14 @@ function SingleProduct({
 }) {
   return (
     <div className="flex flex-wrap gap-10 pb-5">
-      <div className=" object-cover bg-white">
+      <div className="object-cover bg-white">
         <img src={image} alt="" />
       </div>
-      <div className="flex flex-col gap-5 flex-1 ">
-        <h1>{name}</h1>
-        <p>{description}</p>
+      <div className="flex flex-col gap-5 flex-1 max-w-[30rem]">
+        <h1 className="text-4xl font-medium">{name}</h1>
         {rating !== undefined && <StarRating rating={rating} color="text-orange-400"/>}
         <div className="flex flex-col gap-2">
+          <p className="mb-10">{description}</p>
           <p className="font-semibold">{discountedPrice} NOK</p>
           {price !== discountedPrice && (
             <div className="flex gap-2 text-center">
