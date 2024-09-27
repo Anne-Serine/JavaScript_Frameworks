477b32b77d72c740ee7ccad28ef00d684f2544f4
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-09-27 13:56:47 +0200

    Creates search bar functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,36 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useProducts } from "../hooks/Store";
+import { useNavigate } from "react-router-dom";
+
 function Search() {
+  const products = useProducts((state) => state.allProducts);
+  const getAllProducts = useProducts((state) => state.getAllProducts);
+  const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    getAllProducts()
+  }, [getAllProducts]);
+
+  useEffect(() => {
+    if (searchTerm.trim()) {
+      const filtered = products.filter(product =>
+        product.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+      );
+      setFilteredProducts(filtered);
+    } else {
+      setFilteredProducts([]);
+    }
+  }, [searchTerm, products]);
+
+  const handleProductClick = (id => {
+    navigate(`/product/${id}`);
+    setSearchTerm("");
+    setFilteredProducts([]);
+  })
+
   return (
     <div
       className="flex gap-2 items-center relative order-5 md:order-4 w-full md:w-auto"
@@ -15,13 +47,32 @@ function Search() {
         className="w-full p-1 border border-solid border-ecom-dark rounded-sm"
         id="searchInput"
         aria-label="search-input"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      {/* <div>
-        <ul id="searchResults"></ul>
-        <div className="hidden">
-          alert message
-        </div>
-      </div> */}
+      {searchTerm && (
+        <ul id="searchResults" className="absolute top-full left-6 bg-ecom-white border border-solid border-ecom-dark w-full p-1">
+          {filteredProducts.length ? (
+            filteredProducts.map(product => (
+              <li 
+                key={product.id}
+                className="hover:bg-ecom-light hover:font-medium cursor-pointer"
+                onClick={() => handleProductClick(product.id)}
+              >
+                {product.title}
+              </li>
+            ))
+          ) : (
+            <li className="font-medium">No products found</li>
+          )}
+        </ul>
+      )}
+      {/* // <div>
+        
+      //   <div className="hidden">
+      //     alert message
+      //   </div>
+      // </div> */}
     </div>
   )
 }
