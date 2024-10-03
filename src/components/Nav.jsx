dba66f2a016e5f3634e25ad023b2a5ad0ab214e0
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-10-03 20:16:39 +0200

    Change hover in nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,32 @@
 import { Link } from "react-router-dom";
 
-function Nav({isOpen}) {
-  
-  
+function Nav({ isOpen }) {
   return (
-      <nav className={`absolute left-0 top-24 md:top-auto z-10 md:relative md:flex md:order-2 gap-10 container mx-auto bg-ecom-black md:bg-transparent md:flex-nowrap justify-between items-center transition-all duration-300 ${isOpen ? "block" : "hidden"}`}>
-        <ul className="flex flex-col md:flex-row gap-4 md:gap-10">
-          <li>
-            <Link to="/" className="hover:font-semibold hover:text-md hover:border-y hover:py-1 hover:border-ecom-light">Home</Link>
-          </li>
-          <li>
-            <Link to="/contact" className="hover:font-semibold hover:text-md hover:border-y hover:py-1 hover:border-ecom-light">Contact</Link>
-          </li>
-        </ul>
-      </nav>   
-  )
+    <nav
+      className={`absolute left-0 top-24 md:top-auto z-10 md:relative md:flex md:order-2 gap-10 container mx-auto bg-ecom-black md:bg-transparent md:flex-nowrap justify-between items-center transition-all duration-300 ${
+        isOpen ? "block" : "hidden"
+      }`}
+    >
+      <ul className="flex flex-col md:flex-row gap-4 md:gap-10">
+        <li>
+          <Link
+            to="/"
+            className="hover:font-semibold hover:text-md hover:border-y hover:py-1 hover:border-yellow-500"
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/contact"
+            className="hover:font-semibold hover:text-md hover:border-y hover:py-1 hover:border-yellow-500"
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
