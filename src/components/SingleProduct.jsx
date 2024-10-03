dcd674cc3f03690f4b91797df2ca59ce58e884c4
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-10-03 20:18:06 +0200

    Fixes issue with qty on product page and search

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -13,35 +13,56 @@ function SingleProduct({
   discountedPrice,
   price,
 }) {
-
   const addToCart = useCounterStore((state) => state.addToCart);
   const products = useCounterStore((state) => state.products);
-  const [qty, setQty] = useState(1)
+  const [qty, setQty] = useState(1);
 
+  // Reset quantity when product id changes
   useEffect(() => {
-    products.map((obj) => obj.product.id === id && setQty(obj.qty))
-  }, [products, id])
+    setQty(1);
+  }, [id]);
+
+  // If the product is already in the cart, update quantity
+  useEffect(() => {
+    const productInCart = products.find((obj) => obj.product.id === id);
+    if (productInCart) {
+      setQty(productInCart.qty);
+    }
+  }, [products, id]);
 
   return (
     <div className="flex flex-wrap gap-10 pb-5">
       <div className="w-full md:w-7/12">
-        <img src={image} alt="" className="size-full max-h-[40rem] object-cover" />
+        <img
+          src={image}
+          alt=""
+          className="size-full max-h-[40rem] object-cover"
+        />
       </div>
       <div className="flex flex-col gap-5 flex-1 max-w-[30rem]">
         <h1 className="text-4xl font-medium">{name}</h1>
-        {rating !== undefined && <StarRating rating={rating} color="text-orange-400"/>}
+        {rating !== undefined && (
+          <StarRating rating={rating} color="text-orange-400" />
+        )}
         <div className="flex flex-col gap-2">
           <p className="mb-10">{description}</p>
           <p className="font-semibold">{discountedPrice} NOK</p>
           {price !== discountedPrice && (
             <div className="flex gap-2 text-center">
               <p className="line-through">{price} NOK</p>
-              <p className="text-red-400">Save: {(price - discountedPrice).toFixed(2)} NOK</p>
+              <p className="text-red-400">
+                Save: {(price - discountedPrice).toFixed(2)} NOK
+              </p>
             </div>
           )}
         </div>
-        <Counter id={id} qty={qty} setQty={(i) => setQty(i) } />
-        <Button id={id} onClick={(e) => addToCart(e,qty)} text="Add to cart" type="primary" />
+        <Counter id={id} qty={qty} setQty={(i) => setQty(i)} />
+        <Button
+          id={id}
+          onClick={() => addToCart(id, qty)}
+          text="Add to cart"
+          type="primary"
+        />
       </div>
     </div>
   );
