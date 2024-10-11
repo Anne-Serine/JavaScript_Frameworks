8256cad56ea4e72092729d62d4afa7e731b86dcb
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-10-11 16:20:52 +0200

    Adds persist middleware to save cart in local storage

--- src/hooks/Store.jsx
+++ src/hooks/Store.jsx
@@ -1,50 +1,54 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-const useCounterStore = create((set) => ({
-  count: 0,
-  products: [],
-  increment: (id) =>
-    set((state) => ({
-      products: state.products.map((obj) =>
-        obj.product.id === id ? { ...obj, qty: obj.qty + 1 } : obj
-      ),
-    })),
-  decrement: (id) =>
-    set((state) => ({
-      products: state.products.map((obj) =>
-        obj.product.id === id
-          ? { ...obj, qty: obj.qty === 1 ? 1 : obj.qty - 1 }
-          : obj
-      ),
-    })),
-  addToCart: async (id, qty) => {
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL}${id}`).then((res) => res.json());
-    set((state) =>
-      !state.products.some((obj) => obj.product.id === id)
-        ? {
-            count: state.count + qty,
-            products: [...state.products, { product: response.data, qty: qty }],
-          }
-        : {
-            count: state.products.reduce((sum, item) => sum + item.qty, 0),
-            products: state.products,
-          }
-    );
-  },
-  totalCount: (state) => state.products.reduce((acc, obj) => acc + obj.qty, 0),
-  removeItem: (id) => set(
-    (state) => ({
-      count: state.count - state.products.find(obj => obj.product.id === id).qty,
-      products: state.products.filter(obj => obj.product.id !== id)
-    })
-  ),
-  emptyCart: () =>
-    set(() => ({
-      count: 0,
+const useCounterStore = create(
+  persist(
+    (set) => ({
       products: [],
-    })
-  ),
-}));
+      increment: (id) =>
+        set((state) => ({
+          products: state.products.map((obj) =>
+            obj.product.id === id ? { ...obj, qty: obj.qty + 1 } : obj
+          ),
+        })),
+      decrement: (id) =>
+        set((state) => ({
+          products: state.products.map((obj) =>
+            obj.product.id === id
+              ? { ...obj, qty: obj.qty === 1 ? 1 : obj.qty - 1 }
+              : obj
+          ),
+        })),
+      addToCart: async (id, qty) => {
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}${id}`).then((res) => res.json());
+        set((state) =>
+          !state.products.some((obj) => obj.product.id === id)
+            ? {
+                products: [...state.products, { product: response.data, qty: qty }],
+              }
+            : {
+                products: state.products,
+              }
+        );
+      },
+      totalCount: (state) => state.products.reduce((acc, obj) => acc + obj.qty, 0),
+      removeItem: (id) => set(
+        (state) => ({
+          products: state.products.filter(obj => obj.product.id !== id)
+        })
+      ),
+      emptyCart: () =>
+        set(() => ({
+          products: [],
+        })
+      ),
+    }),
+    {
+      name: "product-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
 
 export const useProducts = create((set) => ({
   allProducts: [],
