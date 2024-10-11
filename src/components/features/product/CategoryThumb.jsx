8d155ae66d8050a86dced16f72228794a62aa844
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-10-11 11:47:32 +0200

    Disables/prevent default on click - category link on home page

--- src/components/features/product/CategoryThumb.jsx
+++ src/components/features/product/CategoryThumb.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function CategoryThumb({ name, icon }) {
   return (
-    <Link to="#" className="flex flex-col items-center p-3 gap-4 cursor-not-allowed">
+    <Link to="#" className="flex flex-col items-center p-3 gap-4" onClick={(e) => e.preventDefault()}>
       <div className="bg-ecom-black size-20 flex justify-center items-center rounded-full">
         {icon}
       </div>
